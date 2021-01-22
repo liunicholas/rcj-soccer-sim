f5126e49dcf26ae6f53c890cf77c4cfcbfc201fa
bickolas liu <51180693+liunicholas@users.noreply.github.com>
2021-01-22 15:33:38 -0800

    method for point angle

--- controllers/rcj_soccer_player_b1/utils.py
+++ controllers/rcj_soccer_player_b1/utils.py
@@ -10,3 +10,28 @@ def get_direction(ball_angle: float) -> int:
     if ball_angle >= 345 or ball_angle <= 15:
         return 0
     return -1 if ball_angle < 180 else 1
+
+def getPointAngle(orientation, ball_pos: dict, pointX, pointY) -> Tuple[float, float]:
+    robot_angle: float = orientation
+
+    # Get the angle between the robot and the ball
+    angle = math.atan2(
+        pointY - robot_pos['y'],
+        pointX - robot_pos['x'],
+    )
+
+    if angle < 0:
+        angle = 2 * math.pi + angle
+
+    if robot_angle < 0:
+        robot_angle = 2 * math.pi + robot_angle
+
+    robotPointAngle = math.degrees(angle + robot_angle)
+
+    # Axis Z is forward
+    # TODO: change the robot's orientation so that X axis means forward
+    robotPointAngle -= 90
+    if robotPointAngle > 360:
+        robotPointAngle -= 360
+
+    return robotPointAngle, robot_angle
