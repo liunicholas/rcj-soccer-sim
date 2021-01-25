eaf08bf14aad9262a35761ea2a8a45afe0b74cb9
hapev <evan.coats.21@bishops.com>
2021-01-25 14:11:51 -0800

    worked on attack

--- controllers/rcj_soccer_player_b1/utils.py
+++ controllers/rcj_soccer_player_b1/utils.py
@@ -7,7 +7,6 @@ def get_direction(ball_angle: float) -> int:
     Returns:
         int: 0 = forward, -1 = right, 1 = left
     """
-    if ball_angle >= 345 or ball_angle <= 15:
+    if ball_angle >= 340 or ball_angle <= 20:
         return 0
     return -1 if ball_angle < 180 else 1
-
--- controllers/rcj_soccer_referee_supervisor/referee/consts.py
+++ controllers/rcj_soccer_referee_supervisor/referee/consts.py
@@ -12,7 +12,7 @@ TIME_STEP = 64
 ROBOT_NAMES = ["B1", "B2", "B3", "Y1", "Y2", "Y3"]
 N_ROBOTS = len(ROBOT_NAMES)
 
-BALL_INITIAL_TRANSLATION = [0, 0, 0]
+BALL_INITIAL_TRANSLATION = [-0.5, 0, -0.64]
 
 CENTER_NS = "center_ns"
 YELLOW_LEFT_NS = "yellow_left_ns"
@@ -33,12 +33,12 @@ NEUTRAL_SPOTS = {
 
 
 ROBOT_INITIAL_TRANSLATION = {
-    "B1": [0.3,  0.03817, 0.3],
-    "B2": [0.3,  0.03817, -0.3],
-    "B3": [0.3,  0.03817, 0],
-    "Y1": [-0.3, 0.03817, -0.3],
-    "Y2": [-0.3, 0.03817, 0.3],
-    "Y3": [-0.3, 0.03817, 0],
+    "B1": [1,  0.03817, 0.3],
+    "B2": [0.6,  0.03817, 0],
+    "B3": [1,  0.03817, 0],
+    "Y1": [1, 0.03817, -0.3],
+    "Y2": [1, 0.03817, 0.3],
+    "Y3": [1, 0.03817, 0],
 }
 
 ROBOT_INITIAL_ROTATION = {
@@ -50,8 +50,8 @@ ROBOT_INITIAL_ROTATION = {
     "Y3": [0, 1, 0, 1.57],
 }
 
-BLUE_KICKOFF_TRANSLATION = [0.1, 0.03817, 0]
-YELLOW_KICKOFF_TRANSLATION = [-0.1, 0.03817, 0]
+BLUE_KICKOFF_TRANSLATION = [1, 0.03817, 0]
+YELLOW_KICKOFF_TRANSLATION = [1, 0.03817, 0]
 
 KICKOFF_TRANSLATION = {
     Team.BLUE.value: BLUE_KICKOFF_TRANSLATION,
