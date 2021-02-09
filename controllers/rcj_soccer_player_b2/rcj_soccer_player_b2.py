ae6f017c21edaead157190963a9f241256cf3c0d
hapev <evan.coats.21@bishops.com>
2021-02-08 19:16:43 -0800

    push

--- controllers/rcj_soccer_player_b2/rcj_soccer_player_b2.py
+++ controllers/rcj_soccer_player_b2/rcj_soccer_player_b2.py
@@ -1,159 +1,314 @@
-team = 'BLUE'
-# rcj_soccer_player controller - ROBOT Y2
+team = "YELLOW"
+# rcj_soccer_player controller - ROBOT B3
 
-###### REQUIRED in order to import files from Y1 controller
+###### REQUIRED in order to import files from B1 controller
 import sys
 from pathlib import Path
 sys.path.append(str(Path('.').absolute().parent))
 # You can now import scripts that you put into the folder with your
 # robot B1 controller
-if team == 'BLUE':
+if team == "BLUE":
     from rcj_soccer_player_b1 import rcj_soccer_robot, utils
 else:
     from rcj_soccer_player_y1 import rcj_soccer_robot, utils
-#####
+######
 
 # Feel free to import built-in libraries
 import math
 
 class MyRobot(rcj_soccer_robot.RCJSoccerRobot):
-    def run(self):
-        frameCounter = 0
-        past_position = {"x": 0, "y": 0}
-        x_diff = 1
-        y_diff = 1
-        slope = 0
-        if self.name[0] == 'Y':
-            t_m  = 1
-        else:
-            t_m = -1
-        status = "idling"
-        past_intercept =  {"x": 0, "y": 0}
+
+    def evanMethod(self,ball_pos_last):
+        moving = True
+        shooting = False
+        ball_moving = False
+        waiting_for_ball = False
+
+        xbOLD = 0
+        GETOUT = False
+        team = -1
+
         while self.robot.step(rcj_soccer_robot.TIME_STEP) != -1:
+            if self.name[0] == 'B':
+                team = 1
+
             if self.is_new_data():
                 data = self.get_new_data()
+
+                # Get the position of the ball
+                ball_pos = data['ball']
+                xb = ball_pos['x']
+                if team == 1:
+                    if xb > -0.1:
+                        GETOUT = False
+                    else:
+                        GETOUT = True
+                else:
+                    if xb < 0.1:
+                        GETOUT = False
+                    else:
+                        GETOUT = True
+
+                xbOLD = xb
 
                 # Get the position of our robot
                 robot_pos = data[self.name]
                 # Get the position of the ball
                 ball_pos = data['ball']
-                x_diff = ball_pos['x'] - past_position['x']
-                y_diff = ball_pos['y'] - past_position['y']
-                # if ball_pos['x'] > -0.75 and ball_pos['x'] < -0.59 and ball_pos['y'] > -0.35 and ball_pos['y'] < 0.35:
 
-                if frameCounter % 15 == 0:
-                    if x_diff != 0:
-                        slope = y_diff / x_diff
-                        to_go = self.get_next_position(slope, ball_pos, robot_pos, t_m)
-                        if to_go == {}:
-                            status = "idling"
-                            if robot_pos['y'] < 0.1:
-                                to_go = {'x': -0.56 * t_m, 'y': 0.11}
-                            elif robot_pos['y'] > 0.1:
-                                to_go = {'x': -0.56 * t_m, 'y': -0.11}
-                        elif ball_pos['x'] * t_m < -0.35 and robot_pos['x'] > to_go['x'] - 0.02 and robot_pos['x'] < to_go['x'] + 0.02 and robot_pos['y'] > to_go['y'] - 0.02 and robot_pos['y'] < to_go['y'] + 0.02 and abs(to_go['y'] != 0.11):
-                            to_go = ball_pos
-                            status = "charging"
-                            # print("charging")
-                        elif status == "charging":
-                            if past_intercept['y'] > to_go['y'] - 0.01 and past_intercept['y'] < to_go['y'] + 0.01 and past_intercept['x'] > to_go['x'] - 0.01 and past_intercept['x'] < to_go['x'] + 0.01:
-                                to_go = ball_pos
-                                status = "charging"
-                            else:
-                                status = "idling"
+                if not (ball_pos['x'] == ball_pos_last[0]) and not (ball_pos['y']==ball_pos_last[0]):
+                    ball_moving = True
 
-                    else:
-                        to_go = self.track_ball(ball_pos, t_m)
-                        status = "idling"
-                        if to_go == {}:
-                            if robot_pos['y'] < 0.1:
-                                to_go = {'x': -0.56 * t_m, 'y': 0.11}
-                            elif robot_pos['y'] > 0.1:
-                                to_go = {'x': -0.56 * t_m, 'y': -0.11}
-                past_position = ball_pos
-                past_intercept = to_go
+
+
+                ball_change_x = ball_pos['x'] - ball_pos_last[0]
+                ball_change_y = ball_pos['y'] - ball_pos_last[1]
+
+                # print(ball_change_x, ball_change_y)
+
+
+                # for x in range(10):
+
+
+                robot_angle_2 = robot_pos['orientation']
+
                 # Get angle between the robot and the ball
                 # and between the robot and the north
-                ball_angle, robot_angle = self.get_angles(to_go, robot_pos)
+                ball_angle, robot_angle = self.get_angles(ball_pos, robot_pos)
 
                 # Compute the speed for motors
                 direction = utils.get_direction(ball_angle)
 
+
                 # If the robot has the ball right in front of it, go forward,
                 # rotate otherwise
-                # normal forward speed is 5, but i turned it up to 10 - stanley
+
+
+                rx = robot_pos['x']
+                ry = robot_pos['y']
+                bx = ball_pos['x']
+                by = ball_pos['y']
+
+                if team == 1:
+                    ball_x_dist_from_goal = abs(bx+0.75)
+                else:
+                    ball_x_dist_from_goal = bx-0.75
+
+                ball_y_dist_from_goal = by
+
+                if team == 1:
+                    if (rx-0.05<bx):
+                        moving = True
+                        shooting = False
+                elif (rx+0.05>bx):
+                    moving = True
+                    shooting = False
+
+                # print("byd"+str(ball_y_dist_from_goal))
+
+                # shotx = bx + 0.15/math.sqrt(ball_x_dist_from_goal**2+ball_y_dist_from_goal**2)*ball_x_dist_from_goal + 20*ball_change_x
+                shotx = bx + 0.2/math.sqrt(ball_x_dist_from_goal**2+ball_y_dist_from_goal**2)*ball_x_dist_from_goal
+
+                shoty = by + 0.2/math.sqrt(ball_x_dist_from_goal**2+ball_y_dist_from_goal**2)*ball_y_dist_from_goal+ 10*ball_change_y
+
+                # if shoty > 0.65:
+                #     shoty = shoty = by + 0.2/math.sqrt(ball_x_dist_from_goal**2+ball_y_dist_from_goal**2)*ball_y_dist_from_goal+ 23*ball_change_y
+
+
+                if shoty > 0.25:
+                    shoty = 0.25
+
+                if shoty < -0.25:
+                    shoty = -0.25
+
+                if shotx > 0.65:
+                    shotx = 0.74
+
+                if shotx < -0.65:
+                    shotx = -0.74
+
+                # print(ball_x_dist_from_goal)
+
+                # print(shotx, shoty)
+
+
+                #if not on wall or not behind robot and compensate for movement, improve shooting mechanism (focusing on center of goal instead of ball)
+                xtarget = shotx
+                ytarget = shoty
+
+
+                def moveTo(x,y):
+                    robot_angle_2= robot_pos['orientation']
+
+                    angle = math.atan2(
+                        y - robot_pos['y'],
+                        x - robot_pos['x'],
+                    )
+                    if angle < 0:
+                        angle = 2 * math.pi + angle
+                    if robot_angle_2 < 0:
+                        robot_angle_2 = 2 * math.pi + robot_angle_2
+                    angle2 = math.degrees(angle + robot_angle_2)
+                    angle2 -= 90
+                    if angle2 > 360:
+                        angle2 -= 360
+
+                    d2 = utils.get_direction(angle2)
+                    return d2
+
+
+                if moving:
+                    sp = moveTo(xtarget,ytarget)
+                    if abs(xtarget-rx) < 0.02 and abs(ytarget-ry) < 0.02:
+                        moving = False
+                        # print("DONE")
+                        shooting = True
+                    direction = sp
+
+                if shooting:
+                    if team == 1:
+                        angle = math.atan2(
+                            -robot_pos['y'],
+                            -0.75 - robot_pos['x'],
+                        )
+                    else:
+                        angle = math.atan2(
+                            -robot_pos['y'],
+                            0.75 - robot_pos['x'],
+                        )
+                    if angle < 0:
+                        angle = 2 * math.pi + angle
+                    if robot_angle_2 < 0:
+                        robot_angle_2 = 2 * math.pi + robot_angle_2
+                    angle2 = math.degrees(angle + robot_angle_2)
+                    angle2 -= 90
+                    if angle2 > 360:
+                        angle2 -= 360
+
+                    direction = utils.get_direction(angle2)
+
                 if direction == 0:
                     left_speed = -10
                     right_speed = -10
-                elif direction == 2:
-                    left_speed = 10
-                    right_speed = 10
                 else:
-                    left_speed = direction * 4
-                    right_speed = direction * -4
+                    right_speed = direction * -10
+                    left_speed = direction * 10
 
                 # Set the speed to motors
                 self.left_motor.setVelocity(left_speed)
                 self.right_motor.setVelocity(right_speed)
 
-                frameCounter += 1
-                # if frameCounter % 15 == 0:
-                #     print(past_position)
-                #     print(x_diff)
-                #     print(y_diff)
-                #     print(slope)
-                #     print(x_box_intercept)
-                # print(to_go)
+                ball_pos_last = [ball_pos['x'],ball_pos['y']]
 
-    def get_next_position(self, slope, ball_pos, robot_pos, t_m):
-        b = -slope * ball_pos['x'] + ball_pos['y']
-        if ball_pos['x'] * t_m > -0.59 or ball_pos['y'] * t_m > 0.35 or ball_pos['y'] < -0.35:
-            to_go = self.get_penalty_intercept(slope, ball_pos, b, t_m)
-            if to_go['x'] * t_m == -0.56 and to_go['y'] == 0 and ball_pos['y'] != 0:
-                # dx = -0.75 - ball_pos['x']
-                # print("special")
-                # new_slope = dx / (-ball_pos['y'])
-                # new_b = new_slope * ball_pos['x'] + ball_pos['y']
-                # to_go = self.get_penalty_intercept(new_slope, ball_pos, new_b)
-                to_go = self.track_ball(ball_pos, t_m)
-        else:
-            to_go = {}
-        return to_go
+            if GETOUT:
+                break
 
-    def get_penalty_intercept(self, slope, ball_pos, b, t_m):
-        x_box_intercept = slope * (-0.59 * t_m - ball_pos['x']) + ball_pos['y']
-        if x_box_intercept > 0.65:
-            y_box_intercept = (0.35 - ball_pos['y']) / slope + ball_pos['x']
-            if y_box_intercept * t_m > -0.9 and y_box_intercept * t_m < -.59 and slope > 1:
-                to_go = {"x": y_box_intercept, "y": 0.38}
-            else:
-                to_go = {"x": -0.56 * t_m, "y": 0}
-            # print(x_box_intercept)
-            # print(y_box_intercept)
-        elif x_box_intercept < -0.65:
-            y_box_intercept = (0.35 - ball_pos['y']) / slope + ball_pos['x']
-            if y_box_intercept * t_m > -0.9 and y_box_intercept * t_m < -.59 and slope < 1:
-                to_go = {"x": y_box_intercept, "y": -0.38}
-            else:
-                to_go = {"x": -0.56 * t_m, "y": 0}
-            # print(x_box_intercept)
-            # print(y_box_intercept)
-        elif ball_pos['x'] * t_m > -0.59:
-            to_go = {"x": -0.56 * t_m, "y": x_box_intercept}
-        else:
-            to_go = {"x": -0.56 * t_m, "y": 0}
-        return to_go
+    def run(self):
 
-    def track_ball(self, ball_pos, t_m):
-        if ball_pos['y'] > -0.35 and ball_pos['y'] < 0.35:
-            return {'x': -0.56 * t_m, 'y': ball_pos['y']}
-        elif ball_pos['x'] * t_m > -0.75 and ball_pos['x'] * t_m < -0.59:
-            if ball_pos['y'] > 0:
-                return {'x': ball_pos['x'], 'y': 0.38}
-            elif ball_pos['y'] < 0:
-                return {'x': ball_pos['x'], 'y': -0.38}
-        else:
-            return {}
+        BLOCK = False
+        SPOTONE = True
+        SPOTTWO = False
+        ROAMCLOSE = False
+        xbOLD = 0
+        ybOLD = 0
+        ball_pos_last = [0,0]
+
+        #worth it to take the penalty
+        spotY = 0.1
+
+        while self.robot.step(rcj_soccer_robot.TIME_STEP) != -1:
+            if self.is_new_data():
+                data = self.get_new_data()
+
+                if self.name[0] == 'B':
+                    team = 1
+                else:
+                    team = -1
+
+                if team == 1:
+                    spotX = 0.55
+                else:
+                    spotX = -0.55
+
+                # Get the position of our robot
+                robot_pos = data[self.name]
+                orientation = robot_pos['orientation']
+                xr = robot_pos['x']
+                yr = robot_pos['y']
+
+                # Get the position of the ball
+                ball_pos = data['ball']
+                xb = ball_pos['x']
+                yb = ball_pos['y']
+
+                b1 = data['B1']
+                b3 = data['B3']
+
+                if abs(xr-spotX)<=0.05 and abs(yr-spotY)<=0.05:
+                    SPOTONE = False
+
+                if abs(xr-spotX)<=0.05 and abs(yr+spotY)<=0.05:
+                    SPOTONE = True
+
+                if team == 1:
+                    if xb > 0.15:
+                        BLOCK = True
+                    else:
+                        BLOCK = False
+                else:
+                    if  xb < -0.15:
+                        BLOCK = True
+                    else:
+                        BLOCK = False
+
+                if BLOCK:
+                    self.evanMethod(ball_pos_last)
+
+                elif SPOTONE:
+                    robotPointAngle, robot_angle = utils.getPointAngle(orientation, xr, yr, spotX, spotY)
+
+                    direction = utils.get_direction(robotPointAngle)
+
+                    # If the robot has the ball right in front of it, go forward,
+                    # rotate otherwise
+                    if direction == 0:
+                        left_speed = -10
+                        right_speed = -10
+                    elif direction == -1:
+                        left_speed = direction * 10
+                        right_speed = direction * -10
+                    else:
+                        left_speed = direction * 10
+                        right_speed = direction * -10
+
+                    # Set the speed to motors
+                    self.left_motor.setVelocity(left_speed)
+                    self.right_motor.setVelocity(right_speed)
+
+                else:
+                    robotPointAngle, robot_angle = utils.getPointAngle(orientation, xr, yr, spotX, spotY*-1)
+
+                    direction = utils.get_direction(robotPointAngle)
+
+                    # If the robot has the ball right in front of it, go forward,
+                    # rotate otherwise
+                    if direction == 0:
+                        left_speed = -10
+                        right_speed = -10
+                    elif direction == -1:
+                        left_speed = direction * 10
+                        right_speed = direction * -10
+                    else:
+                        left_speed = direction * 10
+                        right_speed = direction * -10
+
+                    # Set the speed to motors
+                    self.left_motor.setVelocity(left_speed)
+                    self.right_motor.setVelocity(right_speed)
+
+                xbOLD = xb
+                yBOLD = yb
+                ball_pos_last = [ball_pos['x'],ball_pos['y']]
 
 
 my_robot = MyRobot()
